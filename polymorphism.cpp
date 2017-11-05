2c1691d7e503fed37abfab85ddf443de53d2cd4c
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-11-05 15:31:13 +0100

    Update polymorphism.cpp

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -3,6 +3,17 @@
 // Use same function with different outcome
 // The word polymorphism means having many forms.
 
+/*
+Polymorphic class:
+Virtual functions support dynamic binding and object-oriented programming. 
+A class that declares or inherits a virtual function is called a polymorphic class.
+
+Abstract class:
+An abstract class is a class that can be used only as a base class of some other class; 
+no objects of an abstract class can be created except as subobjects of a class derived from it.
+A class is abstract if it has at least one pure virtual function.
+*/	
+
 #include "stdafx.h"
 #include <iostream>
 using namespace std;
